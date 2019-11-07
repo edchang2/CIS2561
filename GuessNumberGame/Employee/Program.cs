08f02838cba71451c7219fde667ceadc20260081
girlt <edchang2@illinois.edu>
2019-11-07 00:30:11 -0600

    adding guess number and employee

---
+++ GuessNumberGame/Employee/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee
+    {
+        private String first;
+        private String last;
+        private double salary;
+
+        public String getFirst()
+        {
+            return first;
+        }
+
+        public String getLast()
+        {
+            return last;
+        }
+
+        public double getSalary()
+        {
+            return salary;
+        }
+
+        public void setFirst(String name)
+        {
+            first = name;
+        }
+
+        public void setLast(String name)
+        {
+            last = name;
+        }
+
+        public Employee(String firstName, String lastName, double amount)
+        {
+            first = firstName;
+            last = lastName;
+            salary = amount;
+        }
+
+        public void setSalary(double amount)
+        {
+            if (amount > 0)
+            {
+                salary = amount;
+            }
+        }
+
+        public void raise(double percentage)
+        {
+            double amount = salary * percentage;
+            salary += amount;
+
+        }
+
+        static void Main(string[] args)
+        {
+            //initialize
+            Employee Matthew = new Employee("Matthew", "Park", 100);
+            Employee Emily = new Employee("Emily", "Dickinson", 150);
+
+            //print salary
+            Console.WriteLine(Matthew.getFirst() + "'s salary is $" + Matthew.getSalary());
+            Console.WriteLine(Emily.getFirst() + "'s salary is $" + Emily.getSalary());
+
+            Console.WriteLine("Raise time");
+            Matthew.raise(.10);
+            Emily.raise(.10);
+
+            Console.WriteLine(Matthew.getFirst() + "'s new salary is $" + Matthew.getSalary());
+            Console.WriteLine(Emily.getFirst() + "'s new salary is $" + Emily.getSalary());
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GuessNumberGame/GuessNumberGame/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumberGame
+{
+    public partial class Form1 : Form
+    {
+        static Random r = new Random();
+        static int number = r.Next(0, 1000);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String text = textBox2.Text;
+            int guess = 0;
+
+            textBox2.Text = "";
+
+            if( Int32.TryParse(text, out guess))
+            {
+                guess = Int32.Parse(text);
+            } else
+            {
+                textBox1.Text += "Input is in an incorrect format\r\n";
+                return;
+            }
+
+            if (guess > number)
+            {
+                textBox1.Text += guess + " is too high\r\n";
+            } else if (guess < number)
+            {
+                textBox1.Text += guess + " is too low\r\n";
+            } else if (guess == number)
+            {
+                textBox1.Text += guess + " is correct\r\n";
+            }
+
+
+
+        }
+    }
+}
